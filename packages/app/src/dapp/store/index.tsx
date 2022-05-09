b9f7b356299407b8673b0a1930643e782739f437
Maxime Aubanel <maximeauba@gmail.com>
2022-05-09 20:27:32 +0200

    add service and types to dapp + add server new functions and mocking

--- packages/app/src/dapp/store/index.tsx
+++ packages/app/src/dapp/store/index.tsx
@@ -1,9 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { reducers } from "./reducers/index";
+import { IAppState, reducers } from "./reducers/index";
+import { dropApi } from "./services/drop";
+import { combineReducers } from "redux";
 
 export const store = configureStore({
-  reducer: reducers,
+  reducer: combineReducers({
+    [dropApi.reducerPath]: dropApi.reducer,
+    ...reducers,
+  }),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(dropApi.middleware),
 });
 
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = IAppState;
 export type AppDispatch = typeof store.dispatch;
