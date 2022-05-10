668b5e860bf1b4c7b2daaff54519922eb079bec1
Maxime Aubanel <maximeauba@gmail.com>
2022-05-10 15:33:22 +0200

    env file + add os api

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,13 +1,30 @@
+import "dotenv/config";
+
 import express, { Application, Request, Response } from "express";
 import axios from "axios";
+import { env } from "process";
 
+// Env setup
+const apiKey = env["apiKey"];
+
+// Express server
 const app: Application = express();
 const port = 3001;
+
+// Axios setup
+axios.defaults.headers.common["X-API-KEY"] = apiKey;
 
 // Body parsing Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/../public"));
+
+type NFT = {
+  img: string;
+  id: number;
+};
+
+type NFTs = { [contract: string]: NFT[] };
 
 type Collection = {
   name: string;
@@ -28,23 +45,6 @@ const DROPS = [
   },
 ];
 
-const fakeAnswer = {
-  Sublimes: [
-    {
-      id: 825,
-      img: "https://lh3.googleusercontent.com/iUN-rqbSEW97A_YNFiUeKuIoQRtIP08w8st9GnBqsQiydLaiF68pGDP4gx-URYmEph_SqXumjZj3TRGxPIFQBR9ZWxDZicur50a8hA=s0",
-    },
-    {
-      id: 437,
-      img: "https://lh3.googleusercontent.com/nlO5vgcrUqgqPlHHrQHrlA5ZsXg2hIxkJkzr72oK9KU9cNojTO8HAxWCOH06xNw0R5_ZocxNuXhLHwbHC282uz63pNnjfQd-mABKEA=s0",
-    },
-    {
-      id: 108,
-      img: "https://lh3.googleusercontent.com/-JDl1IBKkvQtOJKApygolCMXCV09SDAftc5IhqO585oFXvV-XhpaOFEI_WJFJdTJT6l8ypxMb-FlkQGPRNNPN4enIFJoAIpDA2Y7=s0",
-    },
-  ],
-};
-
 app.get("/drop/:drop", async (req: Request, res: Response): Promise<Response> => {
   const drop = DROPS[req.params.drop];
 
@@ -57,19 +57,25 @@ app.get("/drop/:drop/:address", async (req: Request, res: Response): Promise<Res
   const drop = DROPS[req.params.drop as any as number];
   const address = req.params.address;
 
-  // let data = {};
-  // for (let collection of drop.collections) {
-  //   const resq = await axios.get(
-  //     `https://api.opensea.io/api/v1/assets?owner=${address}&asset_contract_address=${collection.contract}&order_direction=desc&offset=0&limit=20`
-  //   );
+  let nftsForDropByAddress: NFTs = {};
 
-  //   data[collection.name] = {
-  //     ...data[collection.name],
-  //     ...resq.data.assets,
-  //   };
-  // }
+  for (let collection of drop.collections) {
+    const resq = await axios.get(
+      `https://api.opensea.io/api/v1/assets?owner=${address}&asset_contract_address=${collection.contract}&order_direction=desc&offset=0&limit=50`
+    );
 
-  return res.status(200).send(fakeAnswer);
+    let nfts: NFT[] = [];
+    for (let asset of resq.data.assets) {
+      nfts.push({
+        img: asset.image_url,
+        id: asset.token_id,
+      });
+    }
+
+    nftsForDropByAddress[collection.contract] = nfts;
+  }
+
+  return res.status(200).send(nftsForDropByAddress);
 });
 
 app.get("/", async (req: Request, res: Response): Promise<Response> => {
