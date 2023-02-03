b839051c655e498fe3d99d13c9b8c071e5d24f5b
Maxime Aubanel <maximeauba@gmail.com>
2023-02-03 19:37:15 +0100

    Update typings

--- packages/typings/dist/index.js
+++ packages/typings/dist/index.js
@@ -47,5 +47,5 @@ exports.ListMockTokens = {
 };
 exports.ChainIdToStoreContract = {
     1337: "0x7fc2C86d96F5046F8056b8C8DDf5aA3849892C21",
-    5: "0x", // Goerli
+    5: "0x8ae7E7B2a1c96A3FACbd916d7EE2BCF3ddF586d0", // Goerli
 };
